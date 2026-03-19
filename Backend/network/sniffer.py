7728a445ccf09b99ec3bbd895586203ece749723
Asad Ali <141873003+Asad-In-Data@users.noreply.github.com>
2026-03-20 01:41:30 +0500

    Log captured packets to CSV

--- Backend/network/sniffer.py
+++ Backend/network/sniffer.py
@@ -2,6 +2,21 @@ from scapy.all import sniff
 from scapy.layers.inet import IP, TCP, UDP
 import csv
 from datetime import datetime
+import os 
+
+# Open CSV file for writing
+if not os.path.exists("network_traffic.csv"):
+    with open("network_traffic.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Time", "Source IP", "Destination IP", "Protocol", "Size"])
+
+
+def write_to_csv(data):    
+    with open("network_traffic.csv", "a", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(data)
+
+
 
 def process_packet(packet):
     if IP in packet:
@@ -16,6 +31,7 @@ def process_packet(packet):
 
         size = len(packet)
 
-        print(f"Source: {src_ip} → Dest: {dst_ip} | Protocol: {protocol} | Size: {size}")
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        write_to_csv([timestamp, src_ip, dst_ip, protocol, size])
 
-sniff(prn=process_packet, store=False)
+sniff(prn=process_packet, store=False,count=50)  # Capture 100 packets, adjust as needed
